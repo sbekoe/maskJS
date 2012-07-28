ef19f0d879f1ed4d25414b516d4f6f310dd49395
sbekoesim <simon@bekoe.net>
2012-07-28 10:23:58 +0200

    build test suite

--- test/test.js
+++ test/test.js
@@ -1,78 +1,77 @@
-var tests = [
-	{
-		name:"full test",
-		template:
-			'<!--defMarker/-->' +
-			'<!--dynMarker/-->' +
-			'<ul class="{{ top }}">\n' +
-				'<!--item-->' +
-				'\t<li class="{{itemClass}}">{{i}}: {{itemName}} - the {{i}} is rendered twice</li>' +
-				'{{unusedMarkerWillDisappear}}' +
-				'<!--/item-->' +
-			'</ul>',
-		options:{
-			pattern:['{{%s%id%s}}', '{{%s%id%s:%tmp%s}}', '<!--%s%id%s/-->', '<!--%s%id%s-->%tmp<!--%s/%id%s-->'],
-			cache:false,
-			marker:{
-				defMarker:"I'm not given in the data object!",
-				dynMarker:function () {
-					return "I'm rendered dynamically by a function!";
-				}
-			}
-		},
-		result:
-			"I'm not given in the data object!"+
-			"I'm rendered dynamically by a function!"+
-			"<ul class="">"+
-				"<li class="li">0: list element - the 0 is rendered twice</li>"+
-				"<li class="li extra class">1: another list element - the 1 is rendered twice</li>"+
-			"</ul>"
+(function($){
+    $(function(){
+        $('.test textarea').keyup(showUnitTest);
+    });
+
+	jQuery.fn.codemirror = function(options){
+		return this.each(function(){$(this).data('codemirror',CodeMirror.fromTextArea(this, options || {}));});
 	}
-];
 
-test( "full test", function() {
-	ok( true, "true succeeds" );
-	ok( "non-empty", "non-empty string succeeds" );
+	jQuery.getJSON('tests.json',{},init);
 
-	ok( false, "false fails" );
-	ok( 0, "0 fails" );
-	ok( NaN, "NaN fails" );
-	ok( "", "empty string fails" );
-	ok( null, "null fails" );
-	ok( undefined, "undefined fails" );
-});
+    function parseOptions(json){
+        var options = {};
+        try{
+            options = eval('('+json+')');
+        }
+        catch(e){
+            console.log('Error while parsing Options: ' + json + ' is no valid object!');
+        }
+        return options;
+    }
 
+    function showUnitTest(editor,event){
+        $('.unitTest').text(
+            JSON.stringify({
+                name:"test",
+                template: $('[name="template"]').data('codemirror').getValue(),
+                result: $('[name="result"]').data('codemirror').getValue(),
+                data: parseOptions($('[name="data"]').data('codemirror').getValue()),
+                options: parseOptions($('[name="options"]').data('codemirror').getValue())
+                //data: $('[name="data"]').data('codemirror').getValue(),
+                //options: $('[name="options"]').data('codemirror').getValue()
+            })
+        );
+    }
 
-var tmp = Mask.t(
-	'<!--defMarker/-->\n' +
-		'<!--dynMarker/-->\n' +
-		'<ul class="{{ top }}">\n' +
-		'<!--item-->\n' +
-		'\t<li class="{{itemClass}}">{{i}}: {{itemName}} - the {{i}} is rendered twice</li> ' +
-		'{{unusedMarkerWillDisappear}}' +
-		'<!--/item-->\n' +
-		'</ul>\n',
-	{
-		pattern:['{{%s%id%s}}', '{{%s%id%s:%tmp%s}}', '<!--%s%id%s/-->', '<!--%s%id%s-->%tmp<!--%s/%id%s-->'],
-		cache:false,
-		marker:{
-			defMarker:"I'm not given in the data object!",
-			dynMarker:function () {
-				return "I'm rendered dynamically by a function!";
+    function init(tests){
+        var t,i,
+			codeMirrorOptions = {
+				lineNumbers:true,
+				onKeyEvent: showUnitTest
+			};
+
+        if(tests.length){
+            t = tests[tests.length-1]; //show last unit test
+            $('[name="template"]')
+				.val(t.template)
+				.codemirror(codeMirrorOptions);
+            $('[name="result"]')
+				.val(t.result)
+				.codemirror(codeMirrorOptions);
+            $('[name="options"]')
+				.val(formatJson(JSON.stringify(t.options)))
+				.codemirror(codeMirrorOptions);
+			$('[name="data"]')
+				.val(formatJson(JSON.stringify(t.data)))
+				.codemirror(codeMirrorOptions);
+        }
+
+		test( "full test", function() {
+			for(i=0; i<tests.length; i++){
+                //console.log(test=tests[i].template,tests[i].options,tests[i].data);
+                var mask = Mask.t(tests[i].template,tests[i].options);
+                //console.log(Mask.t(tests[i].template,{}));
+				equal(mask.render(tests[i].data),tests[i].result,tests[i].name);
+				//ok( true, "true succeeds" );
 			}
-		}
-	}
-);
+		});
 
-console.log(tmp.render({
-	listClass:'ul',
-	itemClass:'li',
-	item:[
-		{itemName:'list element'},
-		{itemClass:'li extra class', itemName:'another list element'}
-	],
-	unusedData:'???'
-}));
-console.log(tmp);
 
-$('.test').after('<pre>["test","' + $('[name="template"]').text().replace(/"/g,'\\"') + '",' + $('[name="options"]').text() + ',"' + $('[name="result"]').text().replace(/"/g,'\\"') + '"</pre>');
+
+        showUnitTest();
+    }
+	// formatJson() :: formats and indents JSON string
+	// from http://ketanjetty.com/coldfusion/javascript/format-json/
+	function formatJson(a){var b="";var c=a;var d=0;var e=c.length;var f="  ";var g="\n";var h="";for(var i=0;i<e;i++){h=c.substring(i,i+1);if(h=="}"||h=="]"){b=b+g;d=d-1;for(var j=0;j<d;j++){b=b+f}}b=b+h;if(h=="{"||h=="["||h==","){b=b+g;if(h=="{"||h=="["){d=d+1}for(var k=0;k<d;k++){b=b+f}}}return b}
+})(jQuery);
